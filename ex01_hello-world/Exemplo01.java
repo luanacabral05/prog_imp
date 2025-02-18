ecb265bed38149e9b32b93f63a4cd4892a25424b
Luana Cabral <163360766+luanacabral05@users.noreply.github.com>
2025-02-18 23:35:54 +0000

    aula 02

---
+++ ex01_hello-world/Exemplo01.java
@@ -0,0 +1,10 @@
+public class Exemplo01 {
+    public static void main(String[] args) {
+        System.out.println("Olá Luana!");
+        System.out.println("O dobro de 10 é " + dobro(10));
+    }
+
+    public static int dobro(int num) {
+        return 2 * num;
+    }
+}
